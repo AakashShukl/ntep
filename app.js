b308ed1ac195368a1dbb9d2b6bb91e15132161a0
Aakash Shukla <shuklaaakash75@gmail.com>
2018-10-19 18:16:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var nodemailer = require('nodemailer');
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.1';
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 var app = express();
 var transporter1= nodemailer.createTransport({
